6239dfc4815100fb0da5353f5e4199d354f8bdf4
Li Dong <cnlidong@live.cn>
2023-03-21 22:04:02 +0800

    更新标题

--- frontend/src/router/permission.ts
+++ frontend/src/router/permission.ts
@@ -12,7 +12,6 @@ export function setupPageGuard(router: Router) {
         next()
       }
       catch (error) {
-        console.log(error)
         if (to.path !== '/500')
           next({ name: '500' })
         else
--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "xyhelper-desktop",
+		Title:  "XYHELPER",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
