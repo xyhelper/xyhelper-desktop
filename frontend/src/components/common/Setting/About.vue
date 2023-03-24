2b35104dd7fa2d35461ead4a18e923dd2a4da3ab
Li Dong <cnlidong@live.cn>
2023-03-24 19:15:54 +0800

    更新说明页

--- frontend/src/components/common/Setting/About.vue
+++ frontend/src/components/common/Setting/About.vue
@@ -1,8 +1,10 @@
 <script setup lang='ts'>
-import { computed, onMounted, ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { NSpin } from 'naive-ui'
 import { fetchChatConfig } from '@/api'
-import { useAuthStore } from '@/store'
+// import pkg from '@/../package.json'
+// import { useAuthStore } from '@/store'
+
 interface ConfigState {
   timeoutMs?: number
   reverseProxy?: string
@@ -14,14 +16,13 @@ interface ConfigState {
   version?: string
 }
 
-const authStore = useAuthStore()
+// const authStore = useAuthStore()
 
 const loading = ref(false)
 
 const config = ref<ConfigState>()
 
-const isChatGPTAPI = computed<boolean>(() => !!authStore.isChatGPTAPI)
-
+// const isChatGPTAPI = computed<boolean>(() => !!authStore.isChatGPTAPI)
 
 async function fetchConfig() {
   try {
@@ -50,11 +51,11 @@ onMounted(() => {
         <p>
           此项目开源于
           https://github.com/xyhelper/xyhelper-desktop
-          ，免费且基于 MIT 协议，没有任何形式的付费行为！
+          ，免费且基于 MIT 协议。
         </p>
         <p>
           如果您觉得这个软件有帮助，请考虑捐赠以支持我们的持续开发工作。
-          <img src="/public/donate.jpg" width="264" height="371">
+         <center> <img src="/donate.jpg" width="264" height="371" /></center>
         </p>
         <p>
           项目界面基于  ChatGPT Web 开发，感谢作者的开源精神！
@@ -64,10 +65,10 @@ onMounted(() => {
       <!-- <p v-if="isChatGPTAPI">
         {{ $t("setting.balance") }}：{{ config?.balance ?? '-' }}
         </p> -->
-        <p v-if="!isChatGPTAPI">
+        <!-- <p v-if="!isChatGPTAPI">
           {{ $t("setting.reverseProxy") }}：{{ config?.reverseProxy ?? '-' }}
         </p>
-        <p>AccessToken：{{ config?.accessToken ?? '-' }}</p>
+        <p>AccessToken：{{ config?.accessToken ?? '-' }}</p> -->
         <!-- <p>{{ $t("setting.timeout") }}：{{ config?.timeoutMs ?? '-' }}</p> -->
         <!-- <p>{{ $t("setting.socks") }}：{{ config?.socksProxy ?? '-' }}</p> -->
         <!-- <p>{{ $t("setting.httpsProxy") }}：{{ config?.httpsProxy ?? '-' }}</p> -->
