f90190960e2772549d9319a3bfa3f1339fc89034
Li Dong <cnlidong@live.cn>
2023-03-26 14:20:51 +0800

    增加新版本检测

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import (
 	"embed"
 
+	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gctx"
+	"github.com/gogf/gf/v2/os/gtime"
+	"github.com/gogf/gf/v2/text/gstr"
+	"github.com/gogf/gf/v2/util/gconv"
 	"github.com/google/uuid"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -20,12 +26,22 @@ var (
 )
 
 func main() {
+	var ctx = gctx.New()
+	var title = "XYHELPER 开源免费的AI助理 https://xyhelper.cn version: " + Version
+	latestVersion, err := GetLatestVersion(ctx)
+	if err == nil {
+
+		if gstr.CompareVersion(latestVersion, Version) > 0 {
+			title = title + " 有新版本 " + latestVersion + " 请前往 https://xyhelper.cn 下载"
+		}
+	}
+
 	// Create an instance of the app structure
 	app := NewApp()
 	AccessToken = uuid.NewString()
 	// Create application with options
-	err := wails.Run(&options.App{
-		Title:  "XYHELPER https://xyhelper.cn version: " + Version,
+	err = wails.Run(&options.App{
+		Title:  title,
 		Width:  1050,
 		Height: 649,
 		AssetServer: &assetserver.Options{
@@ -43,4 +59,19 @@ func main() {
 	if err != nil {
 		println("Error:", err.Error())
 	}
+	g.Log().Info(gctx.New(), "XYHELPER 开源免费的AI助理 https://xyhelper.cn version: "+Version)
+}
+
+func GetLatestVersion(ctx g.Ctx) (lasterVersion string, err error) {
+	r, err := g.Client().Get(ctx, "https://xyhelper.cn/version.txt?time="+gconv.String(gtime.Timestamp()), nil)
+	if err != nil {
+		g.Log().Error(ctx, "GetLatestVersion", err.Error())
+		return
+	}
+	if r.StatusCode != 200 {
+		g.Log().Error(ctx, "GetLatestVersion", "StatusCode!=200")
+		return "", gerror.New("StatusCode!=200")
+	}
+	lasterVersion = gstr.Trim(r.ReadAllString())
+	return
 }
